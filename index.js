363529d8326d8770c3a0e601238b250a1bead8c3
Jigar Dafda <jigar.dafda@gmail.com>
2019-03-13 12:03:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 function handler(messages) {
-console.log('From GitHub')
-                console.log(messages)
-            }
-            module.exports = { handler };
+      console.log('From GitHub');
+      console.log(messages);
+}
+
+function initServer() {
+  const express = require('express')
+  const app = express()
+  const port =  process.env.PORT || 3000;
+
+  app.get('/', (req, res) => res.send('Hello World!'))
+  app.listen(port, () => console.log(`Test app listening on port ${port}!`))
+}
+
+initServer();
+
+module.exports = { handler };
